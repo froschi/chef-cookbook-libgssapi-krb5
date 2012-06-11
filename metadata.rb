9fa849fa089abc62eaf05c9044697807e7289a58
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 14:42:16 +0200

    Added dependant cookbooks.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libcomerr", ">= 0.0.1"
+depends          "libk5crypto", ">= 0.0.1"
+depends          "libkeyutils", ">= 0.0.1"
+depends          "libkrb5", ">= 0.0.1"
+depends          "libkrb5support", ">= 0.0.1"
